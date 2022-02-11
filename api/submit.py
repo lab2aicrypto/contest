48a793b00b5666a0b69fa412a0ea9bd9dc6f6d12
DESKTOP-9DA7AJK\lab2ai <joprr12@lab2ai.com>
2022-02-11 15:07:57 +0900

    예측 결과 제출 함수 업데이트

--- api/submit.py
+++ api/submit.py
@@ -1,15 +1,14 @@
 import requests
-from datetime import datetime, timedelta
+from datetime import datetime
 
 
-def add_feed(mentor_page_id: str, username: str, password: str, coin: str, buy: float, sell: float,
+def add_feed(username: str, password: str, coin: str, buy: float, sell: float,
              predict_minute_range: int, start_time: datetime):
     """
     실시간 가격 예측 결과 제출
 
-    :param password:
-    :param username:
-    :param mentor_page_id: 참가 팀 별 id
+    :param username: 참가팀 별 유저 이름
+    :param password: 참가팀 별 비밀번호
     :param coin: 예측한 가상화폐의 심볼 대문자 문자열. ex) KRW-BTC
     :param buy: 매수가
     :param sell: 매도가
@@ -17,26 +16,21 @@ def add_feed(mentor_page_id: str, username: str, password: str, coin: str, buy: 
     :param start_time: 예측 시작 시간. kst 시간 기준.
     :return:
     """
-    end_time = start_time + timedelta(minutes=predict_minute_range)
-
     params = {
-        "endTime": end_time.strftime("%Y-%m-%d %H:%M:%S"),
-        "importanceValue": 0,
-        "market": coin,
-        "mentorPageId": mentor_page_id,
-        "password": password,
-        "startPrice": buy,
-        "startTime": start_time.strftime("%Y-%m-%d %H:%M:%S"),
-        "targetPrice": sell,
         "username": username,
-        "validateMin": predict_minute_range
+        "password": password,
+        "market": coin,
+        "priceStart": buy,
+        "priceTarget": sell,
+        "timeStart": start_time.strftime("%Y-%m-%dT%H:%M:%S"),
+        "timeValidateMinutes": predict_minute_range
     }
 
-    response = requests.post("http://partner-api.cosign.cc/api/sign/add", data=params)
+    response = requests.post("http://partner-api.cosign.cc/api/sign/v2/add", data=params)
 
     if response.ok:
-        print(f"response: {response}\n"
+        print(f"response: {response.json()}\n"
               f"피드 추가: {params}")
     else:
-        print(f"response: {response}\n"
+        print(f"response: {response.json()}\n"
               f"피드 추가 실패: {params}")
